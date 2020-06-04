1270a797e8505e1e367152dc4f3ede9c16891b02
br34th5 <ek4music@gmail.com>
2020-06-04 13:39:08 +0300

    Scraping Year and Population from one country

--- worldometers/worldometers/spiders/countries.py
+++ worldometers/worldometers/spiders/countries.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import logging
 
 
 class CountriesSpider(scrapy.Spider):
@@ -18,7 +19,17 @@ class CountriesSpider(scrapy.Spider):
             #absolute_url = response.urljoin(link)
             #yield scrapy.Request(url=absolute_url)
             
-            yield response.follow(url=link)
+            yield response.follow(url=link, callback=self.parse_country)
+            
+    def parse_country(self, response):
+        rows = response.xpath('(//table[@class="table table-striped table-bordered table-hover table-condensed table-list"])[1]/tbody/tr')
+        for row in rows:
+            year = row.xpath(".//td[1]/text()").get()
+            population = row.xpath(".//td[2]/strong/text()").get()
+            yield {
+                'year': year,
+                'population': population,
+            }
 
 # to run the spider , terminal: scrapy crawl countries 
 # (virtual_workspace) C:\Users\Einaras\projects\Scraping\worldometers\worldometers>scrapy crawl countries
