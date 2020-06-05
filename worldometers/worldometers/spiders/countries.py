0e2e4499cbcad4edc40e6b6fc41a16b0d5fc9ab1
br34th5 <ek4music@gmail.com>
2020-06-05 17:11:55 +0300

    Now scrapes Country name as well
    country name + year + pop

--- worldometers/worldometers/spiders/countries.py
+++ worldometers/worldometers/spiders/countries.py
@@ -7,6 +7,8 @@ class CountriesSpider(scrapy.Spider):
     name = 'countries'
     allowed_domains = ['www.worldometers.info']
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
+    
+
 
     def parse(self, response):
         countries = response.xpath("//td/a")
@@ -19,14 +21,18 @@ class CountriesSpider(scrapy.Spider):
             #absolute_url = response.urljoin(link)
             #yield scrapy.Request(url=absolute_url)
             
-            yield response.follow(url=link, callback=self.parse_country)
+            #to be able to send data between two parse methods, we have to use REQUESTS META         
+            yield response.follow(url=link, callback=self.parse_country, meta={'country_name': name})
             
     def parse_country(self, response):
+        #meta is a dictionary so we use []
+        name = response.request.meta['country_name']
         rows = response.xpath('(//table[@class="table table-striped table-bordered table-hover table-condensed table-list"])[1]/tbody/tr')
         for row in rows:
             year = row.xpath(".//td[1]/text()").get()
             population = row.xpath(".//td[2]/strong/text()").get()
             yield {
+                'country_name': name,
                 'year': year,
                 'population': population,
             }
