4582e031cfbe42628d78da485ebfa503c4e30c8a
br34th5 <ek4music@gmail.com>
2020-06-07 17:20:39 +0300

    New project is working

--- gdp_debt/gdp_debt/spiders/debt.py
+++ gdp_debt/gdp_debt/spiders/debt.py
@@ -5,27 +5,12 @@ import logging
 class DebtSpider(scrapy.Spider):
     name = 'debt'
     allowed_domains = ['www.worldpopulationreview.com']
-    
-    
-    
+    start_urls = ['https://worldpopulationreview.com/countries/countries-by-national-debt/']
+
     def parse(self, response):
-        countries = response.xpath("//td/a")
-        for country in countries:
-            name = country.xpath(".//text()").get()
-            link = country.xpath(".//@href").get()
-            
-            yield response.follow(url=link, callback=self.parse_country, meta={'country_name': name})
-            
-    
-    def parse_country(self, response):
-        name = response.request.meta['country_name']
-        rows = response.xpath('//table[@class="datatableStyles__StyledTable-bwtkle-1 cyosFW table table-striped"]/tbody/tr')
+        rows = response.xpath("//table/tbody/tr")
         for row in rows:
-            debt = row.xpath('.//td[1]/text()').get()
-            
             yield {
-                'country_name': name,
-                'gdp_debt': debt,
+                'country_name': row.xpath(".//td[1]/a/text()").get(),
+                'gdp_debt': row.xpath(".//td[2]/text()").get()
             }
-            
-            #does not work mehh reik variables pervadint
