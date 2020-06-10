e71a1184ac65a70bdaafe735f0b4e0701c7caeab
br34th5 <ek4music@gmail.com>
2020-06-10 15:35:51 +0300

    Learned how to encode json

--- tinydeal/tinydeal/spiders/special_offers.py
+++ tinydeal/tinydeal/spiders/special_offers.py
@@ -5,7 +5,14 @@ import scrapy
 class SpecialOffersSpider(scrapy.Spider):
     name = 'special_offers'
     allowed_domains = ['www.tinydeal.com']
-    start_urls = ['https://www.tinydeal.com']
+    start_urls = ['https://www.tinydeal.com/specials.html']
 
     def parse(self, response):
-        pass
+        for product in response.xpath("//ul[@class='productlisting-ul']/div/li"):
+            yield {
+                'title': product.xpath(".//a[@class='p_box_title']/text()").get(),
+                'url': response.urljoin(product.xpath(".//a[@class='p_box_title']/@href").get()),
+                'discount_price': product.xpath(".//div[@class='p_box_price']/span[1]/text()").get(),
+                'original_price': product.xpath(".//div[@class='p_box_price']/span[2]/text()").get(),
+            }
+#debugging encoding of json files > settings.py > FEED_EXPORT_ENCODING = 'utf-8'
