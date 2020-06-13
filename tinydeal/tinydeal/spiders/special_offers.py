5cec236bb6fbede7e43fa6354c1714b543741830
br34th5 <ek4music@gmail.com>
2020-06-13 18:03:24 +0300

    Changing user-agent

--- tinydeal/tinydeal/spiders/special_offers.py
+++ tinydeal/tinydeal/spiders/special_offers.py
@@ -5,7 +5,11 @@ import scrapy
 class SpecialOffersSpider(scrapy.Spider):
     name = 'special_offers'
     allowed_domains = ['www.tinydeal.com']
-    start_urls = ['https://www.tinydeal.com/specials.html']
+
+    def start_requests(self):
+        yield scrapy.Request(url='https://www.tinydeal.com/specials.html', callback=self.parse, headers={
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'
+        })
 
     def parse(self, response):
         for product in response.xpath("//ul[@class='productlisting-ul']/div/li"):
@@ -14,11 +18,14 @@ class SpecialOffersSpider(scrapy.Spider):
                 'url': response.urljoin(product.xpath(".//a[@class='p_box_title']/@href").get()),
                 'discount_price': product.xpath(".//div[@class='p_box_price']/span[1]/text()").get(),
                 'original_price': product.xpath(".//div[@class='p_box_price']/span[2]/text()").get(),
+                'User-Agent': response.request.headers['User-Agent']
             }
-#debugging encoding of json files > settings.py > FEED_EXPORT_ENCODING = 'utf-8'
+# debugging encoding of json files > settings.py > FEED_EXPORT_ENCODING = 'utf-8'
 
         next_page = response.xpath("//a[@class='nextPage'/@href").get()
-    
-    # if there is another page it will scrape it 
+
+    # if there is another page it will scrape it
         if next_page:
-            yield scrapy.Request(url=next_page, callback=self.parse)
+            yield scrapy.Request(url=next_page, callback=self.parse, headers={
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'
+            })
