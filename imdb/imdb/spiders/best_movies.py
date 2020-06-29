a2370db10d48eb5ccb73d8d80c4eebc1e1eb1fb6
br34th5 <ek4music@gmail.com>
2020-06-29 18:34:42 +0300

    changing user-agent

--- imdb/imdb/spiders/best_movies.py
+++ imdb/imdb/spiders/best_movies.py
@@ -7,14 +7,26 @@ from scrapy.spiders import CrawlSpider, Rule
 class BestMoviesSpider(CrawlSpider):
     name = 'best_movies'
     allowed_domains = ['imdb.com']
-    start_urls = ['https://www.imdb.com/search/title/?groups=top_250&sort=user_rating']
+    
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
+    
+    def start_requests(self):
+        #no need for call-back method inside request class
+        yield scrapy.Request(url='https://www.imdb.com/search/title/?groups=top_250&sort=user_rating', headers={
+            'User-Agent': self.user_agent
+        })
 
     rules = (
         #allow / deny / restrict_xpaths / restrict_css
-        Rule(LinkExtractor(restrict_xpaths=("//h3[@class='lister-item-header']/a")), callback='parse_item', follow=True),
-        Rule(LinkExtractor(restrict_xpaths="(//a[@class='lister-page-next next-page'])[1]"), follow=True)
+        #adding proccess_request for changing user_agent
+        Rule(LinkExtractor(restrict_xpaths=("//h3[@class='lister-item-header']/a")), callback='parse_item', follow=True, process_request='set_user_agent'),
+        Rule(LinkExtractor(restrict_xpaths="(//a[@class='lister-page-next next-page'])[1]"), follow=True, process_request='set_user_agent')
     )
 
+    def set_user_agent(self, request):
+        request.headers['User-Agent'] = self.user_agent
+        return request
+    
     def parse_item(self, response):
         '''
         item = {}
@@ -30,5 +42,6 @@ class BestMoviesSpider(CrawlSpider):
             'duration': response.xpath("normalize-space((//time)[1]/text())").get(),
             'genre': response.xpath("//div[@class='subtext']/a[1]/text()").get(),
             'rating': response.xpath("//span[@itemprop='ratingValue']/text()").get(),
-            'movie_url': response.url
+            'movie_url': response.url,
+            'user-agent': response.request.headers['User-Agent']
         }
