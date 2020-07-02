b655b8ca9f40ee676dbf257e0f58e0deff62e2fb
br34th5 <ek4music@gmail.com>
2020-07-02 18:53:41 +0300

    project glasshop

--- glasses_shop/glasses_shop/spiders/glassop.py
+++ glasses_shop/glasses_shop/spiders/glassop.py
@@ -5,20 +5,17 @@ from scrapy.spiders import CrawlSpider, Rule
 
 class GlassopSpider(scrapy.Spider):
     name = 'glassop'
-    allowed_domains = ['https://www.glassesshop.com']
-    start_urls = ['https://https://www.glassesshop.com/bestsellers']
+    allowed_domains = ['glassesshop.com']
 
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
     
     def start_requests(self):
         #no need for call-back method inside request class
-        yield scrapy.Request(url='https://https://www.glassesshop.com/bestsellers', headers={
+        yield scrapy.Request(url='https://www.glassesshop.com/bestsellers', headers={
             'User-Agent': self.user_agent
         })
 
     rules = (
-        #allow / deny / restrict_xpaths / restrict_css
-        #adding proccess_request for changing user_agent
         Rule(LinkExtractor(restrict_xpaths=("//div[@class='col-12 pb-5 mb-lg-3 col-lg-4 product-list-row']")), callback='parse_item', follow=True, process_request='set_user_agent'),
         Rule(LinkExtractor(restrict_xpaths="(//li[@class='page-item'][4]/a"), follow=True, process_request='set_user_agent')
     )
@@ -29,6 +26,9 @@ class GlassopSpider(scrapy.Spider):
     
     def parse_item(self, response):
         yield {
-            'title': ,
+            'product_url': response.xpath('//div[@class="product-img-outer"]/a').get(),
+            'product_image_link': response.xpath("//img[@class='lazy w-100 product-img-second']").get(),
+            'product_name': response.xpath('').get(),
+            'product_price': response.xpath('').get()
         }
 
