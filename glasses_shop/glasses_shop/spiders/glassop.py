0bfa3c18c8033efa036c6b5a88568e5270ef1e02
br34th5 <ek4music@gmail.com>
2020-07-10 18:57:15 +0300

    works well

--- glasses_shop/glasses_shop/spiders/glassop.py
+++ glasses_shop/glasses_shop/spiders/glassop.py
@@ -11,19 +11,19 @@ class GlassopSpider(scrapy.Spider):
     #then the original price will be extracted from the XPath expression I used in the else block.
     def get_price(self, selector):
         original_price = selector.xpath(
-            ".//del/text()").get()
+            ".//div[@class='p-price']/div/span/text()").get()
         if original_price is not None:
             return original_price
         else:
-            return selector.xpath(".//div[@class='row']/div[contains(@class, 'pprice')]/span/text()").get()
+            return selector.xpath(".//div[@class='p-price']/div/span[@class='color-red']/text()").get()
 
     def parse(self, response):
-        glasses = response.xpath("//div[contains(@class, 'm-p-product')]")
+        glasses = response.xpath("//div[contains(@class, 'product-list-row')]")
         for glass in glasses:
             yield {
-                'url': glass.xpath(".//div[@class='pimg default-image-front']/a/@href").get(),
-                'img_url': glass.xpath(".//div[@class='pimg default-image-front']/a/img[1]/@src").get(),
-                'name': glass.xpath(".//div[@class='row']/p[contains(@class, 'pname')]/a/text()").get(),
+                'url': glass.xpath(".//div[@class='product-img-outer']/a/@href").get(),
+                'img_url': glass.xpath(".//div[@class='product-img-outer']/a/img[1]/@src").get(),
+                'name': glass.xpath(".//div[@class='p-title']/a/text()").get(),
                 'price': self.get_price(glass)
             }
 
