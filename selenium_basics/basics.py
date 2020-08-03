d145ac2cd3a8b86218d3d785c17ca9fcf1e87485
br34th5 <ek4music@gmail.com>
2020-08-03 18:57:47 +0300

    installed selenium and launched first automated browser

---
+++ selenium_basics/basics.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from shutil import which
+
+chrome_path = which("chromedriver")
+
+
+driver = webdriver.Chrome(executable_path=chrome_path)
+driver.get("https://duckduckgo.com")
