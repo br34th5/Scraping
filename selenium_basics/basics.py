bf928e98cc898e649782788f3ded534fbf4caa22
br34th5 <ek4music@gmail.com>
2020-08-04 18:18:01 +0300

    first executed command by automated  software

--- selenium_basics/basics.py
+++ selenium_basics/basics.py
@@ -6,3 +6,14 @@ chrome_path = which("chromedriver")
 
 driver = webdriver.Chrome(executable_path=chrome_path)
 driver.get("https://duckduckgo.com")
+
+search_input = driver.find_element_by_xpath("(//input[contains(@class, 'js-search-input')])[1]")
+search_input.send_keys("My User Agent")
+"""
+#this is how you find an element with ID
+search_btn.find_element_by_id("search_form_input_homepage")
+driver.find_elements_by_class_name("class")
+driver.find_elements_by_tag_name("h1")
+driver.find_elements_by_xpath("")
+driver.find_elements_by_css_selector()
+"""
