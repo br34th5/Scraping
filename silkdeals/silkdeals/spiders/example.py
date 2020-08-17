7348ffcf8d5db4660f7348af8b70d5caa57004a0
br34th5 <ek4music@gmail.com>
2020-08-17 19:05:10 +0300

    storing page source and then scraping it

--- silkdeals/silkdeals/spiders/example.py
+++ silkdeals/silkdeals/spiders/example.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.selector import Selector
 from scrapy_selenium import SeleniumRequest
 from selenium.webdriver.common.keys import Keys
 
@@ -26,4 +27,12 @@ class ExampleSpider(scrapy.Spider):
         
         #driver.save_screenshot('after_filling_input.png')
         search_input.send_keys(Keys.ENTER)
-        driver.save_screenshot("enter.png")
+        html = driver.page_source
+        response_obj = Selector(text=html)
+        #driver.save_screenshot("enter.png")
+        links = response_obj.xpath('//div[@class="result__extras__url"]/a')
+        for link in links:
+            yield {
+                'URL': link.xpath(".//@href").get()
+            }
+            
