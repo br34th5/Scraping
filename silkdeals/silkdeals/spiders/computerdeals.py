7a1c075110a0936a148b95a7bc04fbfcb5d3ec39
br34th5 <ek4music@gmail.com>
2020-08-18 18:23:23 +0300

    new spider in same project

---
+++ silkdeals/silkdeals/spiders/computerdeals.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy_selenium import SeleniumRequest
+
+class ComputerdealsSpider(scrapy.Spider):
+    name = 'computerdeals'
+
+
+    def parse(self, response):
+        pass
