1386f282764ce5a83faace48103500017ac31a49
br34th5 <ek4music@gmail.com>
2020-08-20 18:40:52 +0300

    not working

--- silkdeals/silkdeals/spiders/computerdeals.py
+++ silkdeals/silkdeals/spiders/computerdeals.py
@@ -4,7 +4,21 @@ from scrapy_selenium import SeleniumRequest
 
 class ComputerdealsSpider(scrapy.Spider):
     name = 'computerdeals'
+    
+    def start_requests(self):
+        yield SeleniumRequest(
+            url='https://slickdeals.net/computer-deals/',
+            wait_time=3,
+            callback=self.parse
+        )
 
 
     def parse(self, response):
-        pass
+        products = response.xpath("//ul[@class='dealTiles categoryGridDeals']/li")
+        for product in products:
+            yield {
+                'name': product.xpath('.//a[@class="itemTitle"]/text()').get(),
+                'link': product.xpath('.//a[@class="itemTitle"]@href').get(),
+                'store_name': product.xpath('.//span[@class="itemStore"]/text()').get(),
+                'price': product.xpath('.//div[@class="itemPrice  wide "]/text()').get()
+            }
