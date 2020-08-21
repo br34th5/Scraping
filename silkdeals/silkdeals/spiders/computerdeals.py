e9b8520b59628cb52e0b5ebf501ddab4fa937901
br34th5 <ek4music@gmail.com>
2020-08-21 18:05:39 +0300

    Splash > Selenium in terms of speed.
    Added pagination and method that removes characters from scraped data

--- silkdeals/silkdeals/spiders/computerdeals.py
+++ silkdeals/silkdeals/spiders/computerdeals.py
@@ -5,6 +5,10 @@ from scrapy_selenium import SeleniumRequest
 class ComputerdealsSpider(scrapy.Spider):
     name = 'computerdeals'
     
+    # method that removes your defined characters from the scraped data
+    def remove_characters(self, value):
+        return value.strip('\xa0')
+    
     def start_requests(self):
         yield SeleniumRequest(
             url='https://slickdeals.net/computer-deals/',
@@ -18,7 +22,16 @@ class ComputerdealsSpider(scrapy.Spider):
         for product in products:
             yield {
                 'name': product.xpath('.//a[@class="itemTitle"]/text()').get(),
-                'link': product.xpath('.//a[@class="itemTitle"]@href').get(),
-                'store_name': product.xpath('.//span[@class="itemStore"]/text()').get(),
-                'price': product.xpath('.//div[@class="itemPrice  wide "]/text()').get()
+                'link': product.xpath('.//a[@class="itemTitle"]/@href').get(),
+                'store_name': self.remove_characters(product.xpath('normalize-space(.//span[@class="itemStore"]/text())').get()),
+                'price': product.xpath('normalize-space(.//div[@class="itemPrice  wide "]/text())').get()
             }
+        
+        next_page = response.xpath('//a[@data-role="next-page"]/@href').get()
+        if next_page:
+            absolute_url = f"https://slickdeals.net{next_page}"
+            yield SeleniumRequest(
+                url=absolute_url,
+                wait_time=3,
+                callback=self.parse
+            )
