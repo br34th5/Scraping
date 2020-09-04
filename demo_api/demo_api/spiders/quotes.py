974d9bb8499208097ff2866c014270375ee93d01
br34th5 <ek4music@gmail.com>
2020-09-04 18:05:08 +0300

    pagination in api

--- demo_api/demo_api/spiders/quotes.py
+++ demo_api/demo_api/spiders/quotes.py
@@ -16,3 +16,12 @@ class QuotesSpider(scrapy.Spider):
                 'tags': quote.get('tags'),
                 'quote_text': quote.get('text')
             }
+        
+        # every API is different, so observe it and try to understand before scraping
+        has_next = resp.get('has_next')
+        if has_next:
+            next_page_number = resp.get('page') + 1
+            yield scrapy.Request(
+                url=f'http://quotes.toscrape.com/api/quotes?page={next_page_number}',
+                callback=self.parse
+            )
